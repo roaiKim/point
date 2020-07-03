048c527b268f34861bb40bf8d8fab59afb11a44d
roaiKim <shcroai@gmail.com>
2020-07-03 10:38:37 +0800

    Create css不常用选择器.css

---
+++ css不常用选择器.css
@@ -0,0 +1,19 @@
+::first-line / 选择文本的首行
+
+::first-letter / 选择首字母
+
+::selection / 选择高亮的区域
+
+:empty / 选择空标签
+
+:only-child / 只有一个子元素
+
+:link / 选择一个未选中的的超链接
+
+:checked / 选择一个选中的复选框
+
+:valid / 选择一个验证的元素---主要配合表单的patter字段
+
+:invaild / 选择一个未验证的元素
+
+:lang() / 选择一个指定语言的元素 <p lang="fr">Paragraph 1</p>   p:lang(fr) {    background: yellow;    }
